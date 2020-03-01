d3f7f7f53db2713d528541648288ddaa07ff4f92
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-03-02 03:47:45 +0600

    total & subtotal added

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-// //plus button event
-// const iphoneB = document.getElementById('plus-btn-1');
-// iphoneB.addEventListener('click', function () {
-//     products('p-value-1', 'phone-price');
-// });
-// const phoneCase = document.getElementById('plus-btn-2');
-// phoneCase.addEventListener('click', function () {
-//     products('p-value-2', 'case-price');
-// });
+//plus button event
+const iphoneB = document.getElementById('plus-btn-1');
+iphoneB.addEventListener('click', function () {
+    products('p-value-1', 'phone-price', 1219);
+});
+const phoneCase = document.getElementById('plus-btn-2');
+phoneCase.addEventListener('click', function () {
+    products('p-value-2', 'case-price', 59);
+});
 
-// function products(setProductNumber, productPrice) {
-//     const p1 = document.getElementById(setProductNumber).value++;
-//     // const parse = Number(p1);
-//     const price1 = document.getElementById(productPrice).innerText;
-//     const convert = parseFloat(price1);
-//     var totalP1;
-//     var totalP2;
-//     // const totalP1 = convert + 1219;
-//     if (productPrice == '1219') {
-//          totalP1 = convert + 1219;
-//     } else if (productPrice == '59') {
-//          totalP2 = convert + 59;
-//     }
-//     document.getElementById(productPrice).innerText = totalP1;
-//     document.getElementById('total').innerText = totalP1 + totalP2;
-//     document.getElementById('subtotal').innerText = totalP1;
-// }
+function products(setProductQuantity, productPrice, p) {
+    /**
+     * setProductQuantity = product quantity from plus button
+     * productPrice = phone/case price 
+     * p = set price of mobile/case
+     */
+    const p1 = document.getElementById(setProductQuantity).value++;
+    const price1 = document.getElementById(productPrice).innerText;
+    const convert = parseFloat(price1);
+    const total = convert + p;
 
+    document.getElementById(productPrice).innerText = total;
+
+    var result = document.getElementById('phone-price').innerText;
+    var result2 = document.getElementById('case-price').innerText;
+
+    var sum = parseFloat(result) + parseFloat(result2);
+    document.getElementById('subtotal').innerText = sum;
+    document.getElementById('total').innerText = sum;
+
+}
